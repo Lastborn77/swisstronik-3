326e8c177b39958df352fe075ee8842ee66ed352
Jeleel Adeyemi <adelekeadeyemi0@gmail.com>
2024-08-04 10:28:17 +0000

    Initial commit

--- scripts/mint.js
+++ scripts/mint.js
@@ -12,7 +12,7 @@ const sendShieldedTransaction = async (signer, destination, data, value) => {
 };
 
 async function main() {
-  const contractAddress = "0x3825014475aE3a34679CC71167bB33a2700E881";
+  const contractAddress = "";
   const [signer] = await hre.ethers.getSigners();
 
   const contractFactory = await hre.ethers.getContractFactory("TestToken");
